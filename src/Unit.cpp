b084dce432d4727d9109ac6e2dcb0cb98457b769
Dev <dsanghan@caltech.edu>
2012-07-07 14:09:24 -0700

    Implemented MoveState Updates.

--- src/Unit.cpp
+++ src/Unit.cpp
@@ -6,7 +6,7 @@ LoggerPtr unitLogger;
 
 Unit::Unit(int64_t playerID) :
     Entity(playerID),
-    state_(new NullState(this))
+    state_(new MoveState(glm::vec3(0.f), this))
 {
     pos_ = glm::vec3(0, 0.5f, 0);
     radius_ = 0.5f;
@@ -16,7 +16,7 @@ Unit::Unit(int64_t playerID) :
 
     unitLogger = Logger::getLogger("Unit");
 
-    vel_ = glm::vec3(0.1f);
+    vel_ = glm::vec3(0.f);
     maxSpeed_ = getParam("unit.maxSpeed");
 }
 
@@ -24,8 +24,7 @@ void Unit::update(float dt)
 {
     state_->update(dt);
 
-    pos_ += glm::vec3(vel_.x * cos (angle_ * M_PI / 180), 0.f,
-					  vel_.z * sin (angle_ * M_PI / 180));
+    pos_ += glm::vec3(vel_.x * dir_.x, 0.f, vel_.z * dir_.y);
 
 }
 
@@ -50,6 +49,29 @@ MoveState::MoveState(const glm::vec3 &target, Unit *unit) :
 
 void MoveState::update(float dt)
 {
+	glm::vec3 diff = (target_ - unit_->pos_);
+	if (abs(diff.x) < 0.05 &&
+		abs(diff.y) < 0.05 &&
+		abs(diff.z) < 0.05)
+	{
+		unit_->vel_ = glm::vec3(0.f);
+	}
+	else {
+		unit_->angle_ = 180 * atan(abs(diff.z) / abs(diff.x)) / M_PI;
+		if (diff.x < 0 && diff.z < 0)
+			unit_->angle_ += 180;
+		else if (diff.x < 0)
+			unit_->angle_ += 90;
+		else if (diff.z < 0)
+			unit_->angle_ += 270;
+		unit_->dir_ = glm::vec2(cos(unit_->angle_*M_PI/180),
+							sin(unit_->angle_*M_PI/180));
+		unit_->vel_ = glm::vec3(0.05f);
+	}
+
+	unitLogger->info() << unit_->angle_ << ": " << diff.x <<"\n";
+
+
 }
 
 UnitState * MoveState::next()
