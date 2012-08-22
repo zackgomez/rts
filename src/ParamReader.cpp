09bdda714a0a374a4d4f34f69629f38a03c036b3
Zack Gomez <zack.gomez@gmail.com>
2012-08-21 18:09:28 -0700

    Added include directive to param file.
    Summary:
    You can now include additional param files in the config by adding a
    comment.  Added local configuration file, but it is currently unused and
    just serves as an example.
    
    Format is `// @include param file.json` which makes the value parsed
    from file.json accesible as `getParam("param")`.
    
    Currently only one level of includes are supported, that is you can only
    include params at the top level.
    
    Test Plan:
     - Compiles, runs, prints loaded message.
     - Verified contents of "local" param.
     - Verified error checking with bad string, bad file and bad namespace
    
    Reviewers: connor
    
    Reviewed By: connor
    
    CC: rskjr
    
    Maniphest Tasks: T79
    
    Differential Revision: http://phabricator.zackgomez.com/D57

--- src/ParamReader.cpp
+++ src/ParamReader.cpp
@@ -13,13 +13,52 @@ ParamReader * ParamReader::get() {
 
 void ParamReader::loadFile(const char *filename) {
   std::ifstream file(filename);
-  std::string data((std::istreambuf_iterator<char>(file)),
-                   std::istreambuf_iterator<char>());
   Json::Reader reader;
-  if (!reader.parse(data, root_)) {
+  if (!reader.parse(file, root_)) {
     logger_->fatal() << "Cannot load param file " << filename << "\n"
                      << reader.getFormattedErrorMessages();
     return;
+  }
+
+  // Read in includes from header comment
+  // format is '// @include param file'
+  std::stringstream commentstream(root_.getComment(Json::commentBefore));
+  std::string comment;
+  while (std::getline(commentstream, comment)) {
+    if (comment.empty()) {
+      continue;
+    }
+    std::vector<std::string> commentsplit;
+    boost::split(commentsplit, comment, boost::is_any_of(" "));
+    // Check format
+    if (commentsplit.size() == 4 && commentsplit[1] == "@include") {
+      const std::string &filename = commentsplit[3];
+      const std::string &paramname = commentsplit[2];
+
+      // Param must not already exist
+      if (root_.isMember(paramname)) {
+        throw exception_with_trace(
+          "asked to include param to already existing name");
+      }
+      // Open file
+      std::ifstream includedfile(filename.c_str());
+      if (!includedfile) {
+        throw exception_with_trace("Unable to open file " + filename);
+      }
+      // Parse
+      Json::Value param;
+      if (!reader.parse(includedfile, param)) {
+        throw exception_with_trace("Unable to read param file " + filename +
+            ": " + reader.getFormattedErrorMessages());
+      }
+
+      root_[paramname] = param;
+
+      LOG(INFO) << "Included file " << filename << " as param " <<
+        paramname << '\n';
+    } else {
+      LOG(WARNING) << "unknown include comment: " << comment << '\n';
+    }
   }
 }
 
@@ -41,7 +80,7 @@ Json::Value ParamReader::getParamHelper(const std::string &param) const {
   Json::Value val = root_;
   std::vector<std::string> param_split;
   boost::split(param_split, param, boost::is_any_of("."));
-for (auto& child : param_split) {
+  for (auto& child : param_split) {
     val = val.get(child, Json::Value());
     if (val.isNull()) {
       break;
