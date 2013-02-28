51d6013228b7e83ceb85062472231fa55eb5baaf
Zack Gomez <zack.gomez@gmail.com>
2013-02-28 02:50:48 -0500

    move ppm files to common/

--- src/exec/depthfieldgen.cpp
+++ src/exec/depthfieldgen.cpp
@@ -3,6 +3,7 @@
 #include <algorithm>
 #include <glm/glm.hpp>
 #include "stb_image.c"
+#include "common/image.h"
 
 const int DSIZE = 256;
 const int RADIUS = 200;
@@ -46,42 +47,6 @@ float *calculate_distance_field(uint8_t *data, int width, int height) {
   }
 
   return depth_field;
-}
-
-void write_pgm(const char *filename, float *data, int width, int height, float min, float max) {
-  FILE *f = fopen(filename, "w");
-  if (!f) {
-    fprintf(stderr, "Unable to open %s for writing\n", filename);
-    exit(1);
-  }
-  fprintf(f, "P2 %d %d 255\n", width, height);
-  for (int y = 0; y < height; y++) {
-    for (int x = 0; x < width; x++) {
-      uint8_t val = 255 * glm::clamp(
-          (data[y * width + x] - min) / (max - min),
-          0.f,
-          1.f);
-      fprintf(f, "%d ", val);
-    }
-    fprintf(f, "\n");
-  }
-  fclose(f);
-}
-
-void write_pgm(const char *filename, uint8_t *data, int width, int height) {
-  FILE *f = fopen(filename, "w");
-  if (!f) {
-    fprintf(stderr, "Unable to open %s for writing\n", filename);
-    exit(1);
-  }
-  fprintf(f, "P2 %d %d 255\n", width, height);
-  for (int y = 0; y < height; y++) {
-    for (int x = 0; x < width; x++) {
-      fprintf(f, "%d ", data[y * width + x]);
-    }
-    fprintf(f, "\n");
-  }
-  fclose(f);
 }
 
 int main(int argc, char **argv) {
