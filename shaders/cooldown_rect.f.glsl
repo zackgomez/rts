d442d46cf674a0fe881e084fd167953337256c3f
Zack Gomez <zack.gomez@gmail.com>
2013-06-02 01:52:00 -0400

    Add missing shader file

---
+++ shaders/cooldown_rect.f.glsl
@@ -0,0 +1,19 @@
+#version 120
+
+uniform vec4 color;
+uniform float cooldown_percent;
+varying vec2 frag_texcoord;
+
+const float PI = 3.1415926;
+
+void main()
+{
+    vec2 p = frag_texcoord - vec2(0.5);
+    float angle = atan(p.x, p.y) / (2 * PI) + 0.5;
+
+    if (angle < cooldown_percent) {
+      discard;
+    }
+
+    gl_FragColor = color;
+}
