6817749b447f0ce7bd22ecf8a67f560693489319
Robert Karl <rkarl@medallia.com>
2013-06-17 23:44:15 -0700

    Fix for referencing global module name in Players module.
    Summary: minor fix to JS namespacing/modular technique
    
    Test Plan: ran the game to completion.
    
    Reviewers: zack
    
    Differential Revision: http://phabricator.zackgomez.com/D157

--- scripts/100-player.js
+++ scripts/100-player.js
@@ -3,60 +3,63 @@
 //
 
 var Players = (function() {
+  // API to return
+  var PlayersAPI = {};
 
+  // Private state
   var players = {};
 
-  return {
-    getPlayerInfo: function(pid) {
-      return players[pid];
-    },
+  PlayersAPI.getPlayerInfo = function(pid) {
+    return players[pid];
+  };
 
-    // Create a new player.
-    // @param pid the player ID
-    // @param start_def dictionary with keys for `pos` and `angle`
-    playerInit: function (pid, starting_def) {
-      if (players[pid]) {
-        throw new Error('player '+pid+' already exists!');
+  // Create a new player.
+  // @param pid the player ID
+  // @param start_def dictionary with keys for `pos` and `angle`
+  PlayersAPI.playerInit = function (pid, starting_def) {
+    if (players[pid]) {
+      throw new Error('player '+pid+' already exists!');
+    }
+
+    var base_id = SpawnEntity(
+      'base',
+      {
+        pid: pid,
+        pos: starting_def.pos,
+        angle: starting_def.angle,
+      });
+    var base_entity = GetEntity(base_id);
+
+    var unit_id = SpawnEntity(
+      'unit',
+      {
+        pid: pid,
+        pos: vecAdd(base_entity.getPosition2(), base_entity.getDirection()),
+        angle: base_entity.getAngle(),
+      });
+
+    players[pid] = {
+      units: {
+        base: base_id,
+        unit: unit_id,
       }
+    };
+  };
 
-      var base_id = SpawnEntity(
-        'base',
-        {
-          pid: pid,
-          pos: starting_def.pos,
-          angle: starting_def.angle,
-        });
-      var base_entity = GetEntity(base_id);
-
-      var unit_id = SpawnEntity(
-        'unit',
-        {
-          pid: pid,
-          pos: vecAdd(base_entity.getPosition2(), base_entity.getDirection()),
-          angle: base_entity.getAngle(),
-        });
-
-      players[pid] = {
-        units: {
-          base: base_id,
-          unit: unit_id,
-        }
-      };
-    },
-
-    playerUpdate: function (player) {
-      for (var entity_name in player.units) {
-        var entity = GetEntity(player.units[entity_name]);
-        if (!entity) {
-          delete player.units[entity_name];
-        }
-      }
-    },
-
-    updateAllPlayers: function () {
-      for (var pid in players) {
-        this.playerUpdate(players[pid]);
+  PlayersAPI.playerUpdate = function (player) {
+    for (var entity_name in player.units) {
+      var entity = GetEntity(player.units[entity_name]);
+      if (!entity) {
+        delete player.units[entity_name];
       }
     }
   };
+
+  PlayersAPI.updateAllPlayers = function () {
+    for (var pid in players) {
+      PlayersAPI.playerUpdate(players[pid]);
+    }
+  };
+
+  return PlayersAPI;
 })();
