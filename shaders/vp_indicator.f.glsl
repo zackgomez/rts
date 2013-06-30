0d77ea9d5a78ba41643b7c26d5e5fb915af40532
Connor DeFanti <connor@Connors-MacBook-Pro-2.local>
2013-06-30 16:27:14 -0700

    Fixed some effects on scorebar, added diffuse highlights to vp indicators

--- shaders/vp_indicator.f.glsl
+++ shaders/vp_indicator.f.glsl
@@ -13,9 +13,13 @@ void main()
     vec2 p = 2 * (frag_texcoord - vec2(0.5));
     float radius2 = dot(p, p);
     if (radius2 > 1) discard;
+    vec3 normal = vec3(p.x, p.y, sqrt(1 - radius2));
+    // some arbitrary light position, maybe use a uniform?
+    vec3 lightdir = normalize(vec3(0.25, -0.25, 1.0));
     if (2 * capture - 1.0 > -p.y) {
       color = cap_color;
     }
+    float highlight = max(0, dot(normal, lightdir));
 
-    gl_FragColor = color;
+    gl_FragColor = color + highlight * vec4(0.5);
 }
