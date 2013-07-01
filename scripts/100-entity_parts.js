d5e91b56357ec8df495c27cb132136f40b2ff260
Zack Gomez <zack.gomez@gmail.com>
2013-06-30 22:18:58 -0400

    a bit of work towards part effects

--- scripts/100-entity_parts.js
+++ scripts/100-entity_parts.js
@@ -1,10 +1,21 @@
-
+//
+// -- Core part class definition
 function Part (params) {
   if (!params.health) {
     throw new Error("Missing part health");
   }
   this.maxHealth_ = params.health;
   this.health_ = this.maxHealth_;
+
+  this.deadUpdate_ = params.dead_update
+    ? param.dead_update
+    : function (entity) {};
+
+  this.aliveUpdate = params.alive_update
+    ? param.alive_update
+    : function (entity) {};
+
+  // member functions
 
   this.getHealth = function () {
     return this.health_;
@@ -22,8 +33,20 @@ function Part (params) {
     );
     return this.health_;
   }
+
+  this.update = function (entity) {
+    if (this.health_ <= 0) {
+      this.deadUpdate_(entity);
+    } else {
+      this.aliveUpdate_(entity);
+    }
+  }
 }
 
 function makePart(params) {
   return new Part(params);
 }
+
+// 
+// -- Part update functions
+//
