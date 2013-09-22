00109d85ba58045ecdc649f07d0aef501956c4b6
Zack Gomez <zack.gomez@gmail.com>
2013-09-22 02:07:50 -0400

    add missing file

---
+++ jscore/bootstrap.js
@@ -0,0 +1,87 @@
+// This function is called with the native runtime object to start up the game
+// engine
+//
+// This file largely ripped from the Node.js minimal module system and startup
+// process
+(function (runtime) {
+  function bootstrap() {
+    var invariant = NativeModule.require('invariant').invariant;
+    var main = NativeModule.require('main').main;
+    Log('main', typeof main);
+    invariant(
+      typeof main === 'function',
+      'main module must export a function \'main\''
+    );
+
+    main();
+  }
+
+  function NativeModule(id) {
+    this.filename = id + '.js';
+    this.id = id;
+    this.exports = {};
+    this.loaded = false;
+  }
+
+  NativeModule._source = runtime.source_map;
+  NativeModule._cache = {};
+
+  NativeModule.require = function(id) {
+    if (id === 'native_module') {
+      return NativeModule;
+    }
+
+    var cached = NativeModule.getCached(id);
+    if (cached) {
+      return cached.exports;
+    }
+
+    if (!NativeModule.exists(id)) {
+      throw new Error('No such native module ' + id);
+    }
+
+    var nativeModule = new NativeModule(id);
+
+    nativeModule.cache();
+    nativeModule.compile();
+
+    return nativeModule.exports;
+  };
+
+  NativeModule.getCached = function(id) {
+    return NativeModule._cache[id];
+  }
+
+  NativeModule.exists = function(id) {
+    return NativeModule._source.hasOwnProperty(id);
+  }
+
+  NativeModule.getSource = function(id) {
+    return NativeModule._source[id];
+  }
+
+  NativeModule.wrap = function(script) {
+    return NativeModule.wrapper[0] + script + NativeModule.wrapper[1];
+  };
+
+  NativeModule.wrapper = [
+    '(function (exports, require, module, __filename) {',
+    '\n});'
+  ];
+
+  NativeModule.prototype.compile = function() {
+    var source = NativeModule.getSource(this.id);
+    source = NativeModule.wrap(source);
+
+    var fn = runtime.eval(source, {filename: this.filename});
+    fn(this.exports, NativeModule.require, this, this.filename);
+
+    this.loaded = true;
+  };
+
+  NativeModule.prototype.cache = function() {
+    NativeModule._cache[this.id] = this;
+  };
+
+  bootstrap();
+});
