8e10866a998e5dabf9c01d95913847486d1162e1
Sangkyu Kim <SangkyuKim@zjzjrjr-mac.local>
2014-01-21 20:31:49 -0600

    changed teleport radius/mana.

--- scripts/EntityDefs.js
+++ scripts/EntityDefs.js
@@ -172,10 +172,10 @@ var EntityDefs = {
     },
     actions: {
       teleport: new Actions.TeleportAction({
-        range: 8.0,
+        range: 30.0,
         cooldown_name: 'teleport',
         cooldown: 2.0,
-        mana_cost: 5,
+        mana_cost: 40,
         icon: 'teleport_icon',
         hotkey: 'q',
         part: 'body',
